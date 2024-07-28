8a60c1a301a54bc699471627cd116e697f863890
Jessica Yu <jessicayu62@gmail.com>
2024-07-28 01:11:10 -0700

    update projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import Card from "../components/Card"
-import portfolio from "../assets/portfolio.png";
+import spinder from "../assets/spinder.png";
 import pomodoroTimer from "../assets/pomodoro_timer.png";
 import Container from 'react-bootstrap/Container';
 
 export default function Projects() {
-    const portfolio_desc = () => {
+    const spinder_desc = () => {
         return (
-            <h6>A portfolio created with React and Bootstrap to showcase my experience, projects, and more.
-                Click <a href="https://jessicayu62.github.io/" target="_blank" rel="noopener noreferrer" style={{ color: 'black', textDecoration: 'none' }}><em>here</em></a> to view my original website before revamping to what you see now!
-                This project will continue to be updated as I expand my skill set.</h6>
+            <h6>A React web app that uses Spotify's Web API to suggest music recs based on users' top artists + tracks.
+                As users listen to 30-second clips of tracks, they can heart them to directly add to their Liked Songs playlist on Spotify.
+                Check it out <a href="https://spinder-listen.netlify.app/" target="_blank" rel="noopener noreferrer" style={{ color: 'black', textDecoration: 'none' }}><em>here</em></a> !
+            </h6>
         )
     };
 
@@ -27,10 +28,10 @@ export default function Projects() {
                 <h4>Projects</h4>
                 <Container className="project-content">
                     <Card
-                        image={portfolio}
-                        title="Portfolio"
-                        description={portfolio_desc()}
-                        link="https://github.com/jessicayu62/portfolio-website" />
+                        image={spinder}
+                        title="Spinder"
+                        description={spinder_desc()}
+                        link="https://github.com/jessicayu62/spinder" />
                     <Card
                         image={pomodoroTimer}
                         title="Pomodoro Timer"
